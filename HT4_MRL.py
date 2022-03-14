71cb324e65d9b0a9ad788eeb51f27ad488fc4b15
Raúl Angel Jiménez <46568595+raulangelj@users.noreply.github.com>
2022-03-14 15:10:46 -0600

    Add files via upload

---
+++ HT4_MRL.py
@@ -0,0 +1,10 @@
+# %% [markdown]
+# # HOJA DE TRABAJO 4 MODELOS DE REGRESION LINEAL
+
+# Raul Jimenez 19017
+
+# Donaldo Garcia 19683
+
+# Oscar Saravia 19322
+
+# link al repo: https://github.com/raulangelj/HT4_MRL
